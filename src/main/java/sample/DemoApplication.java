710c49e1296be0f122cb9585aa6944da7c5c7705
shinyay <shinya.com@gmail.com>
2018-09-17 16:21:25 +0900

    Add annotation for Spring Boot Admin Servcer

--- src/main/java/sample/DemoApplication.java
+++ src/main/java/sample/DemoApplication.java
@@ -1,10 +1,12 @@
 package sample;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableAdminServer
 @SpringBootApplication
 public class DemoApplication {
 
