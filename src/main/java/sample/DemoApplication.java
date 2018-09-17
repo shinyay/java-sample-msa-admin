27adc7780f2eaf49058573011527a21c7847a4a6
shinyay <shinya.com@gmail.com>
2018-09-17 16:54:31 +0900

    Remove Microservice dashboard dependency and annotation

--- src/main/java/sample/DemoApplication.java
+++ src/main/java/sample/DemoApplication.java
@@ -1,6 +1,5 @@
 package sample;
 
-import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableAdminServer
-@EnableMicroservicesDashboardServer
 @SpringBootApplication
 public class DemoApplication {
 
